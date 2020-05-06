9133984d4527f5b37c1e742de7d8830550b27743
Mohamed-a591 <62339055+Mohamed-a591@users.noreply.github.com>
2020-05-06 22:41:22 +0200

    intial

---
+++ precache-manifest.ad67313fb8e5805507cc275896b070cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a089118050026acbae79ef3b6c20e47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "706e05be425a4d0fa23d",
+    "url": "/static/css/main.8ceac8d9.chunk.css"
+  },
+  {
+    "revision": "bfbd1c2e3598e05a9495",
+    "url": "/static/js/2.465ccc4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.465ccc4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706e05be425a4d0fa23d",
+    "url": "/static/js/main.386e6e11.chunk.js"
+  },
+  {
+    "revision": "99a1df5724a42cc4fef3",
+    "url": "/static/js/runtime-main.4a3acf61.js"
+  }
+]);
